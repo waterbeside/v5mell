1b4bc7e5be733016aa68774041dcc0b92dbc65c5
Kan <waterbeside@gmail.com>
2017-06-14 01:14:19 +0800

    加载数据完成

--- src/pages/list_herb_result.vue
+++ src/pages/list_herb_result.vue
@@ -3,37 +3,24 @@
     <div class="heading h7">结果</div>
     <div class="body">
       <ul class="me-list-herb">
-        <li>
-          <h5 class="title">紫苏
-            <div class="me-tag me-tag-hc-12">温</div>
-            <div class="me-tag">辛</div>
+        <li v-for="item in result_list">
+          <h5 class="title">{{item.title}}
+            <herb-tags type='hot_cold' :num="item.warm_cold"></herb-tags>
+            <herb-tags type='odor' :num="item.odor"></herb-tags>
+
           </h5>
-          <div class="latin">1.Perilla frutescens(L.)Britt. Var .crispa (Thumb.) Hand.-Mand .-Mazz.2.Perilla frutescens (L.) Britt.var.acuta (Thunb.) Kudo</div>
-          <div class="summary">为唇形科植物紫苏和野紫苏的叶或带叶小软枝。 功效：解表散寒，行气和胃。用于风寒感冒，咳嗽呕恶，妊娠呕吐，鱼蟹中毒。</div>
-        </li>
-        <li>
-          <h5 class="title">澤瀉
-            <div class="me-tag me-tag-hc-8">寒</div>
-            <div class="me-tag">甘</div>
-          </h5>
-          <div class="latin">Alisma orientale (Sam.) Juz. [A. plantago-aquatica L. var. orientale Sam.]</div>
-          <div class="summary">为泽泻科植物泽泻的块茎。</div>
+          <div class="latin">{{item.latin_name}}</div>
+          <div class="summary">{{item.description}}</div>
         </li>
 
+
       </ul>
-      <loading :hideLoading="hideLoading"></loading>
-      <div class="me-showMore-btn">
-        加载更多
+
+      <div class="me-loadMore-btn" @click="loadMore">
+        <loading :hideLoading="hideLoading"></loading>
+        {{text_showMore}}
       </div>
-      <div class="me-pagination  pagination-sm">
-        <a class="page-link disabled ">上一页</a>
-        <div class="page-jump">
-          当前第
-          <input type="number" class="page-input" value="1" min="1" max="80">/80
-          页
-        </div>
-        <a class="page-link">下一页</a>
-      </div>
+
     </div>
   </section>
 </template>
@@ -46,31 +33,71 @@ export default {
   data () {
     return {
       result_list : [],
+      total:0,
       keyword:'',
       page:1,
-      hideLoading:1
+      pagecount:0,
+      text_showMore:'加载更多',
+      hideLoading:1,
     }
   },
   components:{
     herbTags,loading
   },
   methods:{
-    getListDatas (keyword,page){
+    getListDatas (page) {
       var that = this;
-      this.hideLoading = 0
-      this.$ajax.get(config.api.herb_result,{params:{keyword,page}}).then(function(response){
-        console.log(response);
-        that.hideLoading = 1;
-        that.result_list = response.datas;
-      }, function(error){
+      var keyword = this.keyword;
+      this.hideLoading = 0;
 
+      this.$ajax.get(config.api.herb_result,{params:{keyword,page}}).then(function(response){
+        that.hideLoading = 1;
+        if(page==1){
+          if(response.data.datas.length == 0){
+            that.text_showMore = "檢得 0 條數據。"
+          }else{
+              var pagecount = Math.ceil(response.data.total / response.data.pagesize);
+              that.result_list = response.data.datas;
+              that.page = response.data.page;
+              that.total = response.data.total;
+              that.pagecount = pagecount;
+              that.text_showMore = pagecount == 1 ? "全部加載完畢，已無更多": "加載更多";
+          }
+        }else{
+          var list = that.result_list;
+          list = list.concat(response.data.datas);
+          that.page = page;
+          that.result_list = list;
+        }
+
+      }, function(error){
+        var that = this;
+        that.text_showMore = "加載失敗，請檢查您的網絡";
       })
+    },
+    loadMore () {
+      if(this.total == 0 ){
+        return false;
+      }
+      if (this.pagecount > this.page && this.hideLoading){
+        var page = parseInt(this.page) + 1 ;
+        this.getListDatas(page);
+      }else{
+        this.text_showMore = "全部加载完毕，已无更多";
+      }
+    },
+    onScroll () {
+      console.log(1)
     }
   },
   mounted () {
-    let keyword = this.$route.params.keyword;
-    this.keyword = keyword ;
-    this.getListDatas(keyword,1)
+    this.keyword = this.$route.params.keyword;
+    this.getListDatas(1)
+  },
+  created () {
+    this.$nextTick(function () {
+     window.addEventListener('scroll', this.onScroll)
+    })
   }
 }
 </script>
