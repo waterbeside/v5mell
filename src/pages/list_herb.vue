1759f4c9340fabb15c81529c6e9d0b5630c86c78
Kan <waterbeside@gmail.com>
2017-06-14 09:52:43 +0800

    添加历史记录

--- src/pages/list_herb.vue
+++ src/pages/list_herb.vue
@@ -5,7 +5,7 @@
 
          <div class="search-form-wrap ">
            <form @submit="submitForm">
-               <input type="search" placeholder="Search for..." class="form-control form-control-sm form-control-icon" ref="j-search-input" @focus="focusInputAction">
+               <input type="search" placeholder="Search for..." class="form-control form-control-sm form-control-icon" ref="j-search-input" @focus="focusInputAction"  v-model='keyword'>
                <i class="fa fa-times-circle" @click="clearInput"></i>
                </form>
          </div>
@@ -18,12 +18,11 @@
     <section class="me-st-box" v-show="showHistory">
       <div class="heading h7">历史查询</div>
       <div class="body">
-        <ul class="me-search-history">
-          <li>紫苏</li>
-          <li>测试</li>
+        <ul class="me-search-history" >
+          <li v-for="item in history_list" @click="submitHistory">{{item}}</li>
         </ul>
         <div class="me-btn-wrap">
-          <a class="btn btn-secondary ">清除历史记录</a>
+          <a class="btn btn-secondary " @click="clearHistory">清除历史记录</a>
         </div>
       </div>
     </section>
@@ -43,7 +42,9 @@
 export default {
   data () {
     return {
-      showHistory : 0
+      showHistory : 0,
+      history_list: [],
+      keyword :''
     }
   },
 
@@ -58,17 +59,52 @@ export default {
           this.showHistory = 1;
       }
     },
+    submitKeyword (keyword) {
+      this.addHistory(keyword)
+      this.$router.push({name: 'list_result',params: { keyword: keyword }});
+      this.showHistory = 0;
+    },
     submitForm () {
       let $searchInput = this.$refs['j-search-input'];
       var keyword = $searchInput.value;
       if(keyword){
         $searchInput.blur();
-        this.$router.push({name: 'list_result',params: { keyword: keyword }});
-        this.showHistory = 0;
+        this.submitKeyword(keyword);
       }
+    },
+    submitHistory (event) {
+      var keyword = event.currentTarget.innerText
+      this.keyword = keyword;
+      this.submitKeyword(keyword);
+    },
+    addHistory: function (data) {
+      var that = this;
+      var hisDataString = localStorage.getItem('herb_history') || '';
 
+      var hisData = hisDataString == '' ? [] : JSON.parse(hisDataString);
+      var hisData_temp = hisData;
+
+      if (hisData && hisData.length > 0) {
+        hisData.map(function (item, index) {
+          if (item == data || index > 15 ) {
+            hisData_temp.splice(index, 1);
+          }
+        });
+        hisData_temp.unshift(data);
+      }else{
+        hisData_temp = [data]
+      }
+      this.history_list = hisData_temp;
+      var storageData = JSON.stringify(hisData_temp) ;
+      localStorage.setItem('herb_history',storageData);
+
+    },
+    clearHistory : function(){
+      this.history_list = [];
+      localStorage.setItem('herb_history',null);
     }
   },
+
   mounted () {
     if(this.$route.name === "list_herb"){
       this.showHistory = 1;
@@ -76,7 +112,10 @@ export default {
     }else{
       this.showHistory = 0;
     }
-
+  },
+  created () {    
+    var data = JSON.parse(localStorage.getItem('herb_history'));
+    this.history_list = data;
   }
 }
 </script>
